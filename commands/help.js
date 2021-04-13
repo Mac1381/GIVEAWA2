5debada7b3b8ae72928b23d3d570c20f7a407b0d
Mac1381 <81661150+Mac1381@users.noreply.github.com>
2021-04-13 14:44:33 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     if(!message.content.startsWith(prefix)) return;
 
     let help = new Discord.MessageEmbed()
-      .setAuthor("PĤÃŇtØM🥀")
+      .setAuthor("Mac Giveaways")
       .setTitle("Command List & Guide for the Bot")
       .setDescription("Below are Commands you can do with Bot, Right now there is only 6 commands available, more commands will be added soon.")
       .addField("🎁 Giveaway 🎁","~start [channel-name] [Time] [winners] [Prize]\nreroll [prize name]\nend [prize name]")
@@ -17,7 +17,7 @@ module.exports.run = async (client, message, args) => {
       .addField("Check out", "[This Channel](https://www.youtube.com/channel/UCtxWom0HSeOnGqXLTgGopMw) to make your own giveaway bot")
       .setTimestamp()
       .setFooter(`Command Requested By ${message.author.tag}`, client.user.displayAvatarURL());
-    message.channel.send("**Sent the commands in Direct Messages! 💌, Check DMs**");
+    message.channel.send("**Sent the commands in Direct Messages! , Check DMs**");
 
     return message.author.send(help);
 }
